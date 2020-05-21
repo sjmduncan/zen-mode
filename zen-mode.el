60f69742b8cb94ca2fa1170a5ce805cf96278758
Stu D <stu@sjmd.dev>
2020-05-21 21:59:00 +1200

    Fix window resize hook, set text width instead of margins, default to 80 cols wide

--- zen-mode.el
+++ zen-mode.el
@@ -1,4 +1,4 @@
-;;; zen-mode.el --- Zen Editing Mode
+;;; zen-mode.el --- Zen Editing Mode for emacs
 
 ;; Copyright (C) 2017 Akilan Elango
 
@@ -7,7 +7,6 @@
 ;; X-URL: https://github.com/aki237/zen-mode
 ;; URL: https://github.com/aki237/zen-mode
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -39,161 +38,114 @@
 
 
 ;; variables
-(defvar zen-mode-disable-mode-line t
-  "Zen mode option to hide the mode line.  Set true to hide the mode line in Zen-mode.")
+(defvar zen-mode:disable-mode-line t "Hide the mode line in zen-mode.")
+(defvar zen-mode:zen-cols 81 "Default width of zen mode text area (columns).")
 
 ;; local variables
-(defvar-local zen-run-mode nil
-  "zen-run-mode variable is to set state of whether zen mode is enabled in the current buffer"
-  )
+(defvar-local zen-run-mode nil "Set to true when zen mode is enabled")
 
-(defvar-local zen-mode-margin-width (/ (/ (display-pixel-width) (/ (window-pixel-width) (window-width))) 5)
-  "Zen mode option to specify the margin width."
-  )
-
-(defvar-local zen-mode-line-cache nil
-  "zen-mode-line-cache variable is used to save the modeline format content for future when it is disabled"
-  )
-
-(defvar-local zen-menubar-state nil
-  "zen-menubar-state variable is used to save whether menu-bar-mode is enabled before entering the zen mode"
-  )
-
-(defvar-local zen-toolbar-state nil
-  "zen-toolbar-state variable is used to save whether tool-bar-mode is enabled before entering the zen mode"
-  )
-
-(defvar-local zen-linum-state nil
-  "zen-linum-state variable is used to save whether linum-mode is enabled before entering the zen mode"
-  )
-
-(defvar-local zen-blink-cursor-state nil
-  "zen-blink-cursor-state variable is used to save whether blink-cursor-mode is enabled before entering the zen mode"
-  )
-
-(defvar-local zen-lm-state 0
-  "zm-lm-state variable is used to store the left margin width before the activation of zen-mode"
-  )
-
-(defvar-local zen-rm-state 0
-  "zm-rm-state variable is used to store the right margin width before the activation of zen-mode"
-  )
-
-
-(defvar-local zen-lf-state 0
-  "zm-lf-state variable is used to store the left fringe width before the activation of zen-mode"
-  )
-
-(defvar-local zen-rf-state 0
-  "zm-rf-state variable is used to store the right fringe width before the activation of zen-mode"
-  )
-
-(defvar-local zen-fullscreen-state nil
-  "zm-fullscreen-state variable is used to store fullscreen state before the activation of zen-mode"
-  )
+;; Variables for saving the pre-zen state of the window, and restoring it when disabling zen mode
+(defvar-local prezen-modeline nil "Pre-zen modeline format")
+(defvar-local prezen-menubar nil "Pre-zen menubar enabled-disabled")
+(defvar-local prezen-toolbar nil "Pre-zen toolbar enabled-disabled")
+(defvar-local prezen-linum nil "Pre-zen line number mode")
+(defvar-local prezen-cursorblink nil "Pre-zen cursor blink")
+(defvar-local prezen-lm 0 "Pre-zen left margin")
+(defvar-local prezen-rm 0 "Pre-zen right margin")
+(defvar-local prezen-lf 0 "Pre-zen left margin")
+(defvar-local prezen-rf 0 "Pre-zen right margin")
+(defvar-local prezen-fullscreen nil "Pre-zen fullscreen state")
 
 ;;;###autoload
 (define-minor-mode zen-mode
   "zen-mode is a distraction free editing mode for Emacs"
   :lighter " Zen"
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-c C-<") 'zen-mode-increase-margin-width)
-            (define-key map (kbd "C-c C->") 'zen-mode-decrease-margin-width)
+            (define-key map (kbd "C-<") 'zen-mode:decrease-text-width)
+            (define-key map (kbd "C->") 'zen-mode:increase-text-width)
             map)
-  (zen-run)
-  )
+  (zen-toggle))
 
 ;; functions
-(defun zen-run()
+(defun zen-toggle()
   "zen-run is a lisp function which activates and deactivates the zen-mode"
   (if zen-run-mode
-      (progn
-        (message "Distraction free disabled")
-        (setq window-size-change-functions (remove 'zen-mode-refresh-buffer 'window-size-change-functions))
-	(if zen-linum-state
-	    (linum-mode 1))
-	
-	(if zen-blink-cursor-state
-          (blink-cursor-mode 1))
+			(zen-mode:zen-off)
+		(zen-mode:zen-on)))
 
-	(if zen-mode-disable-mode-line
-          (progn
-            (setq mode-line-format zen-mode-line-cache)))
+(defun zen-mode:zen-on()
+	"Turn zen-mode on"
+	(setq prezen-modeline mode-line-format)
+	(setq prezen-menubar (and (boundp 'menu-bar-mode) menu-bar-mode))
+	(setq prezen-toolbar (and (boundp 'tool-bar-mode) tool-bar-mode))
+	(setq prezen-linum (and (boundp 'linum-mode) linum-mode))
+	(setq prezen-cursorblink (and (boundp 'cursor-blink-mode) cursor-blink-mode))
+	(setq prezen-lm left-margin-width)
+	(setq prezen-rm right-margin-width)
+	(setq prezen-lf left-fringe-width)
+	(setq prezen-rf right-fringe-width)
+	(setq prezen-fullscreen (frame-parameter (selected-frame) 'fullscreen))
 
-	(if zen-toolbar-state
-	    (tool-bar-mode 1))
+	(add-hook 'window-size-change-functions 'zen-mode:refresh-buffer)
+	(set-frame-parameter nil 'fullscreen 'fullboth)
 
-	(if zen-menubar-state
-	    (menu-bar-mode 1))
-	(setq left-fringe-width zen-lf-state)
-	(setq right-fringe-width zen-rf-state)
-	(setq left-margin-width zen-lm-state)
-        (setq right-margin-width zen-rm-state)
-        (set-frame-parameter nil 'fullscreen 'zen-fullscreen-state)
-        (set-window-buffer nil (current-buffer))
-        (setq zen-run-mode nil))
-    (progn
-      (if zen-mode-disable-mode-line
-          (progn
-            (setq zen-mode-line-cache mode-line-format)
-            (setq mode-line-format nil)))
+	(linum-mode -1)
+	(menu-bar-mode -1)
+	(tool-bar-mode -1)
+	(blink-cursor-mode -1)
+	(setq left-fringe-width 0)
+	(setq right-fringe-width 0)
 
-      (setq zen-menubar-state (and (boundp 'menu-bar-mode) menu-bar-mode))
+	(if zen-mode:disable-mode-line
+		 (setq mode-line-format nil))
 
-      (setq zen-toolbar-state (and (boundp 'tool-bar-mode) tool-bar-mode))
+	(zen-mode:set-margins)
+	(setq zen-run-mode t)
+	(message "Distractions Removed"))
 
-      (setq zen-linum-state (and (boundp 'linum-mode) linum-mode))
+(defun zen-mode:zen-off()
+	"Turn zen-mode off"
+	(setq zen-run-mode nil)
+	(remove-hook 'window-size-change-functions 'zen-mode:refresh-buffer)
+	(if prezen-menubar (menu-bar-mode 1))
+	(if prezen-toolbar (tool-bar-mode 1))
+	(if prezen-linum (linum-mode 1))
+	(if prezen-cursorblink (cursor-blink-mode))
 
-      (setq zen-blink-cursor-state (and (boundp 'blink-cursor-mode) blink-cursor-mode))
 
-      (setq zen-lm-state left-margin-width)
+	(setq left-fringe-width prezen-lf)
+	(setq right-fringe-width prezen-rf)
+	(setq left-margin-width prezen-lm)
+	(setq right-margin-width prezen-rm)
+	(set-frame-parameter nil 'fullscreen prezen-fullscreen)
+	(set-window-buffer nil (current-buffer))
+	(message "Distractions are back")
+	)
 
-      (setq zen-rm-state right-margin-width)
-
-      (setq zen-lf-state left-fringe-width)
-
-      (setq zen-rf-state right-fringe-width)
-
-      (setq zen-fullscreen-state (frame-parameter (selected-frame) 'fullscreen))
-      
-      (message "Distraction free enabled")
-      (add-to-list 'window-size-change-functions 'zen-mode-refresh-buffer)
-      (set-frame-parameter nil 'fullscreen 'fullboth)
-      (linum-mode -1)
-      (setq right-fringe-width 0)
-      (setq left-fringe-width 0)
-      (menu-bar-mode -1)
-      (tool-bar-mode -1)
-      (blink-cursor-mode -1)
-      (zen-mode-set-margins)
-      (setq zen-run-mode t))))
-
-(defun zen-mode-increase-margin-width()
-  "zen-mode-increase-margin-width is a interactive lisp function to increase the margin width in zen-mode (by 5 units)."
+(defun zen-mode:decrease-text-width()
+  "Decrease zen text width by two columns"
   (interactive)
-  (setq zen-mode-margin-width (+ zen-mode-margin-width 5))
-  (zen-mode-set-margins)
+  (setq zen-mode:zen-cols (- zen-mode:zen-cols 2))
+  (zen-mode:set-margins)
   )
 
-(defun zen-mode-decrease-margin-width()
-  "zen-mode-increase-margin-width is a interactive lisp function to increase the margin width in zen-mode (by 5 units)."
+(defun zen-mode:increase-text-width()
+  "Increase zen text width by two columns"
   (interactive)
-  (setq zen-mode-margin-width (+ zen-mode-margin-width -5))
-  (zen-mode-set-margins)
+  (setq zen-mode:zen-cols (+ zen-mode:zen-cols 2))
+  (zen-mode:set-margins)
   )
 
-(defun zen-mode-set-margins()
-  "zen-mode-set-margins is a lisp function to set the margin width (both left and right)."
-  (setq left-margin-width zen-mode-margin-width)
-  (setq right-margin-width zen-mode-margin-width)
+(defun zen-mode:set-margins()
+  "zen-mode:set-margins is a lisp function to set the margin width (both left and right)."
+  (setq left-margin-width (/ (- (window-total-width) zen-mode:zen-cols) 2))
+  (setq right-margin-width (/ (- (window-total-width) zen-mode:zen-cols) 2))
   (set-window-buffer nil (current-buffer))
   )
 
-(defun zen-mode-refresh-buffer(frame)
-  "zen-mode-refresh-buffer is a lisp function to refresh the buffer's margin width when window is resized"
-  (setq zen-mode-margin-width (/ (/ (display-pixel-width) (/ (window-pixel-width) (window-width))) 5))
-  (zen-mode-set-margins)
-  )
+(defun zen-mode:refresh-buffer(frame)
+  "zen-mode:refresh-buffer is a lisp function to refresh the buffer's margin width when window is resized"
+	(zen-mode:set-margins))
 
 (provide 'zen-mode)
 
